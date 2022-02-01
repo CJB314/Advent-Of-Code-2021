cf1ad1e564592e3a1383c8129ac085cc18c93f18
Corrupt10n404 <52146696+Corrupt10n404@users.noreply.github.com>
2022-02-01 12:17:14 -0800

    Fixed require statement

--- Day2/Day2.js
+++ Day2/Day2.js
@@ -1,4 +1,4 @@
-let [x,p] = [y=z=0,require("./util.js").getData()]
+let [x,p] = [y=z=0,require("../util.js").getData()]
 p.forEach(i=>{
     if(i.startsWith("f")) x += (z += parseInt(i.strip(/.* /))*y)*0 + parseInt(i.strip(/.* /))
     else if(i.startsWith("d")) y += parseInt(i.strip(/.* /))
