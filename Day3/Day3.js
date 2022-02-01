120e8c29d982cb9ff771c9f55b91c16e3fa9e92f
Corrupt10n404 <52146696+Corrupt10n404@users.noreply.github.com>
2022-02-01 12:16:26 -0800

    Fixed require statement

--- Day3/Day3.js
+++ Day3/Day3.js
@@ -1,5 +1,5 @@
 let z = function(n=true,z=false){
-    let p = require("./util.js").getData()
+    let p = require("../util.js").getData()
     for(let [j,v,k] = [0,[[],[]],"",[0,0,0]]; j < p[0].length; [j,v] = [j+1,[[],[]]]){
         for(let i = 0; i < p.length; i++) v[parseInt(p[i][j])]++
         if(!((p = z? (p.filter(e=>{ return (((v[1] >= v[0] ? (n?1:0) : (n?0:(v[1] == p.length/2 ? 0: 1)))+ (e[j] == '1')) != 1 || p.length == 1)})) : p)*0) && !((k += (v[1] >= v[0] ? (n?1:0) : (n?0:(v[1] == p.length/2 ? 0: 1))))*0) && j==(p[0].length-1)) return {k,p}
